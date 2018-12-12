ed551fc55cbcc338f83a8d7c87fc09affcda4a6d
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-12 09:35:07 -0700

    fixes feature test and add search controller"

--- spec/features/user_can_see_stations_spec.rb
+++ spec/features/user_can_see_stations_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 feature 'User can search for the closest fuel stations' do 
   scenario 'they can search via zip code' do 
-    zip_code: 80203
+    zip_code = 80203
     # When I visit "/"
     visit '/'
     # And I fill in the search form with 80203 (Note: Use the existing search form)
     # And I click "Locate"
-    fill_in :search, with: zip_code
+    fill_in :q, with: zip_code
     click_on 'Locate'
     # Then I should be on page "/search"
-    expect(current_path).to eq(/search)
+    expect(current_path).to eq('/search')
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     # And the stations should be limited to Electric and Propane
     # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
