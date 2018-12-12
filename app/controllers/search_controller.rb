1e47c7f13c2cd05e5388b3d7417827da23fbf218
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-12-12 09:46:42 -0700

    adds index action and facade"

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ApplicationController
   def index
-    
+    @facade = StationFacade.new(params[:zip_code])
   end
 end
---
+++ app/facades/station_facade.rb
@@ -0,0 +1,9 @@
+class StationFacade 
+  
+  def initialize(zip_code)
+    @zip_code = zip_code
+  end
+  
+  def stations
+  end
+end 
